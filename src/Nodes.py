5e8566bed0123d8f28da721c8b857e5d2961415f
HariSeldon91 <carlos.davila.luelmo@gmail.com>
2019-06-10 21:38:50 +0200

    Datos a json y representacion de nodos

---
+++ src/Nodes.py
@@ -0,0 +1,48 @@
+
+# Dataset de usuarios y followers
+
+import networkx as nx #Libreria de las redes
+import matplotlib.pyplot as plt #Libreria para la representacion de redes
+import pandas as pd
+import json
+
+orig_user = pd.read_csv("userdata/PDVR.csv")
+orig_user.columns = ['PDVR']
+
+full_edge_list = []
+for user in orig_user['PDVR']:
+    # Lee cada archivo
+    filename = str(user) + ".csv"
+    path = os.path.join(os.getcwd(),"userdata") # Concatena el nombre de archivo
+    file = os.path.join(path,filename)
+    df = pd.read_csv(file, index_col=None)
+    
+    # Crea la lista de followers
+    followers_list = list(df['username'])
+    data = {user : followers_list}
+    
+    full_edge_list.append(data)
+
+    result = {}
+    for d in full_edge_list:
+        result.update(d)
+
+
+with open('nodes.json', 'w') as fp:
+    json.dump(result, fp)
+
+
+with open("nodes.json", "r") as edge_list:
+    data = json.load(edge_list)
+    G = nx.Graph()
+
+for user in data.keys():
+    G.add_node(user)
+    for follower in data.values():
+        G.add_edge(user, str(follower))
+
+#Se dibuja la gráfica    
+nx.draw(G, node_color='r', edge_color='y')
+
+#Se pinta la gráfica
+plt.show()
